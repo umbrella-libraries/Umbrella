f9fca564d98b00fd8d880da75439d4eb3d9dce3f
Richard Edwards <richard.edwards@remarkable.global>
2026-01-19 15:14:25 +0000

    Add AzureFrontDoorVerificationToken header constant
    Introduced the AzureFrontDoorVerificationToken constant to UmbrellaForwardedHeaderNames. This represents the "X-AFD-Verification-Token" header, which can be used to verify requests originate from Azure Front Door. Includes XML documentation for clarity.

--- Core/src/Umbrella.WebUtilities/Http/Constants/UmbrellaForwardedHeaderNames.cs
+++ Core/src/Umbrella.WebUtilities/Http/Constants/UmbrellaForwardedHeaderNames.cs
@@ -6,12 +6,21 @@
 public static class UmbrellaForwardedHeaderNames
 {
 	/// <summary>
-	/// The name of the header that contains the original host that the request was made to
-	/// when the request has been forwarded by Azure Front Door.
+	/// The name of the header that contains the original host that the request was made to when the request has been
+	/// forwarded by Azure Front Door.
 	/// </summary>
 	/// <remarks>
-	/// This is a custom header which must be manually added to the request by Azure Front Door
-	/// using the Rule Sets feature. It is not added automatically by Azure Front Door.
+	/// This is a custom header which must be manually added to the request by Azure Front Door using the Rule Sets
+	/// feature. It is not added automatically by Azure Front Door.
 	/// </remarks>
 	public const string AzureFrontDoorForwardedHost = "X-AFD-Fowarded-Host";
+
+	/// <summary>
+	/// Represents the HTTP header name used for Azure Front Door verification tokens.
+	/// </summary>
+	/// <remarks>
+	/// This header can be used when validating requests to ensure they originate from Azure Front Door and are not
+	/// direct requests.
+	/// </remarks>
+	public const string AzureFrontDoorVerificationToken = "X-AFD-Verification-Token";
 }
