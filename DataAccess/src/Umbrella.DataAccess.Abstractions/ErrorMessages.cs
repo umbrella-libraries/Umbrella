03d33ab73b3c6ab7f0cfc7ac0da59857c7c3ade2
Richard Edwards <richard.edwards@ultimedia.agency>
2026-01-20 13:36:07 +0000

    Refactor error message formats for .NET 8 support
    ErrorMessages now uses CompositeFormat for error message formats on .NET 8 or greater, improving string formatting efficiency. For earlier .NET versions, formats remain as string constants. Internal constant strings introduced to reduce duplication and enhance maintainability. Bulk action concurrency message is always a string constant.

--- DataAccess/src/Umbrella.DataAccess.Abstractions/ErrorMessages.cs
+++ DataAccess/src/Umbrella.DataAccess.Abstractions/ErrorMessages.cs
@@ -1,27 +1,52 @@
-﻿namespace Umbrella.DataAccess.Abstractions;
+﻿#if NET8_0_OR_GREATER
+using System.Text;
+#endif
+
+namespace Umbrella.DataAccess.Abstractions;
 
 /// <summary>
 /// Common error messages used by EF repositories.
 /// </summary>
 public static class ErrorMessages
 {
-	/// <summary>
-	/// The invalid property string length error message format
-	/// </summary>
-	public const string InvalidPropertyStringLengthErrorMessageFormat = "The {0} value must be between {1} and {2} characters in length.";
-
-	/// <summary>
-	/// The invalid property number range error message format
-	/// </summary>
-	public const string InvalidPropertyNumberRangeErrorMessageFormat = "The {0} value must be between {1} and {2}.";
+	private const string InvalidPropertyStringLengthErrorMessageFormatInternal = "The {0} value must be between {1} and {2} characters in length.";
+	private const string InvalidPropertyNumberRangeErrorMessageFormatInternal = "The {0} value must be between {1} and {2}.";
+	private const string ConcurrencyExceptionErrorMessageFormatInternal = "A concurrency error has occurred whilst trying to save the item with id {0} or one of its dependants.";
 
 	/// <summary>
 	/// The bulk action concurrency exception error message
 	/// </summary>
 	public const string BulkActionConcurrencyExceptionErrorMessage = "A concurrency error has occurred whilst trying to update the items.";
 
+#if NET8_0_OR_GREATER
+	/// <summary>
+	/// The invalid property string length error message format
+	/// </summary>
+	public static readonly CompositeFormat InvalidPropertyStringLengthErrorMessageFormat = CompositeFormat.Parse(InvalidPropertyStringLengthErrorMessageFormatInternal);
+
+	/// <summary>
+	/// The invalid property number range error message format
+	/// </summary>
+	public static readonly CompositeFormat InvalidPropertyNumberRangeErrorMessageFormat = CompositeFormat.Parse(InvalidPropertyNumberRangeErrorMessageFormatInternal);
+
 	/// <summary>
 	/// The concurrency exception error message format
 	/// </summary>
-	public const string ConcurrencyExceptionErrorMessageFormat = "A concurrency error has occurred whilst trying to save the item with id {0} or one of its dependants.";
+	public static readonly CompositeFormat ConcurrencyExceptionErrorMessageFormat = CompositeFormat.Parse(ConcurrencyExceptionErrorMessageFormatInternal);
+#else
+	/// <summary>
+	/// The invalid property string length error message format
+	/// </summary>
+	public const string InvalidPropertyStringLengthErrorMessageFormat = InvalidPropertyStringLengthErrorMessageFormatInternal;
+
+	/// <summary>
+	/// The invalid property number range error message format
+	/// </summary>
+	public const string InvalidPropertyNumberRangeErrorMessageFormat = InvalidPropertyNumberRangeErrorMessageFormatInternal;
+
+	/// <summary>
+	/// The concurrency exception error message format
+	/// </summary>
+	public const string ConcurrencyExceptionErrorMessageFormat = ConcurrencyExceptionErrorMessageFormatInternal;
+#endif
 }
